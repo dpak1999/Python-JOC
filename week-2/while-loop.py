47f70931452a7eebb8d12bbbfd2f7102a291d644
deepak kumar dash <deepakkumardash313@gmail.com>
2020-02-04 15:12:49 +0530

    while loop

---
+++ week-2/while-loop.py
@@ -0,0 +1,12 @@
+print("Hello, Everyone have a wonderful day!!!!")
+n=1
+flag=1
+while(flag==1):
+    print("Token nummber {} may please come in".format(n))
+    flag=int(input("Do you want to continue?[0/1]"))
+    n+=1
+print("Thank you Everyone")
+    
+    
+    
+    
